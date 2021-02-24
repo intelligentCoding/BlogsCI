643f331de53a359666172291e36b77ff000ad175
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-02-23 19:35:43 -0800

    more travis configuration done

--- config/dev.js
+++ config/dev.js
@@ -4,4 +4,5 @@ module.exports = {
   googleClientSecret: '-rnmgQNtFYjJ0a47AH4wlJ2i',
   mongoURI: 'mongodb+srv://kasjee4ujee:kasjee4ujee@cluster0.zjv87.mongodb.net/sample_airbnb?retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl : 'redis://127.0.0.1:6379'
 };
